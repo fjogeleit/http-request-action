b497d195dc2df9401e2ca6f1a6ee0489ff595178
uzlopak <aras.abbasi@googlemail.com>
2022-11-04 10:28:14 +0100

    use strict

--- dist/index.js
+++ dist/index.js
@@ -4967,6 +4967,9 @@ exports["default"] = _default;
 /***/ 8169:
 /***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
 
+"use strict";
+
+
 const core = __nccwpck_require__(2186);
 
 class GithubActions {
@@ -5012,6 +5015,9 @@ module.exports = { GithubActions, LogActions }
 
 /***/ 9082:
 /***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
+
+"use strict";
+
 
 const axios = __nccwpck_require__(8757);
 const FormData = __nccwpck_require__(4334)
@@ -5137,7 +5143,7 @@ const convertToJSON = (value) => {
  * @returns {FormData}
  */
 const convertToFormData = (data, files) => {
-  formData = new FormData()
+  const formData = new FormData()
 
   for (const [key, value] of Object.entries(data)) {
     formData.append(key, value)
@@ -9166,8 +9172,11 @@ module.exports = JSON.parse('{"application/1d-interleaved-parityfec":{"source":"
 /******/ 	
 /************************************************************************/
 var __webpack_exports__ = {};
-// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+// This entry need to be wrapped in an IIFE because it need to be in strict mode.
 (() => {
+"use strict";
+
+
 const core = __nccwpck_require__(2186);
 const axios = __nccwpck_require__(8757);
 const https = __nccwpck_require__(5687);
